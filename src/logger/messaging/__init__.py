2e9d41d978f225602f2e41c41af785a82977c44e
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2025-11-30 23:27:21 +0100

    Fixed init

--- src/logger/messaging/__init__.py
+++ src/logger/messaging/__init__.py
@@ -1,4 +1,4 @@
-from .consumer import LISTENING_QUEUES, PUBLIC_KEY
-from .global_vars import RABBITMQ_CONFIG
+from .consumer import LISTENING_QUEUES
+from .global_vars import RABBITMQ_CONFIG, PUBLIC_KEY
 
 __all__ = ["LISTENING_QUEUES", "PUBLIC_KEY", "RABBITMQ_CONFIG"]
